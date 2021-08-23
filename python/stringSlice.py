d737a5d96f875c0cc7d388ed8b693d6698ddb140
Tilak Poudel <tilak@dzangolab.com>
2021-08-23 19:04:04 +0545

    feat: python strings

---
+++ python/stringSlice.py
@@ -0,0 +1,16 @@
+message = 'Hello from Krishna....'
+
+# slicing text from string 
+print(message[0:4]) # extract text from positionn 0 to 4 ie exclude position 5
+print(message[6:10]) # from
+print(message[6:22]) # from Krishna....
+print(message[6:]) # from Krishna....
+print(message[0:]) # Hello from Krishna....
+print(message[:6]) # Hello
+print(message[:]) # Hello
+
+# negative indexing
+message = 'Hello from Balaram'
+
+print(message[-3:-1]) # 
+
