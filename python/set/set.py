99f80bfacafdca7138501dfe7b39ebe637987d26
Tilak Poudel <tilak@dzangolab.com>
2021-09-03 19:48:42 +0545

    feat: set

---
+++ python/set/set.py
@@ -0,0 +1,13 @@
+'''
+Sets are used to store multiple items in a single variable.
+Set is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Tuple, and Dictionary, all with different qualities and usage.
+
+A set is a collection which is both unordered and unindexed.
+Set items are unordered, unchangeable, and do not allow duplicate values.
+
+Sets are written with curly brackets{}.
+'''
+
+fruits = {'apple', 'banana', 'cherry'}
+print(type(fruits))
+print((fruits)
