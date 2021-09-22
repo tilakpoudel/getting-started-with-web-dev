f965380296af9b3f3ab7c8a0a19c2a87e9d3e0d3
Tilak Poudel <tilak@dzangolab.com>
2021-09-22 20:12:17 +0545

    feat: user input

---
+++ python/function/input.py
@@ -0,0 +1,13 @@
+'''
+User Input in python
+'''
+
+name = 'hari'
+myname = input('Enter your name.')
+address = input('Enter your address.')
+
+print('Hello i am '+ myname + ' from ' +address)
+print(name)
+
+number = input('enter any number .')
+print(number)
