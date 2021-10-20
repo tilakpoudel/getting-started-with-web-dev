3362f92b3b704933fcbac62cf2ba5b8f6ee20394
Tilak Poudel <tilak@dzangolab.com>
2021-10-20 19:22:52 +0545

    feat: palindrome

---
+++ python/practise/palindrome.py
@@ -0,0 +1,47 @@
+'''
+Q1.
+WAP in to check wether the input string is palindrome or not using function.
+
+eg: madam >> palindrome
+    hari >> not palindrome
+    mom >> palindrome 
+    dad >> palindrome 
+    eye >> palindrome
+
+Q2. 
+Write a program to find the number of trailing zeroes in a given factorial.
+
+Example 1:
+
+Input: 4
+Output: 0
+Explanation: 4!(4*3*2*1) = 24, no trailing zero.
+
+Example 2:
+Input: 6
+Output: 1
+Explanation: 6!(6*5*4*3*2*1) = 720, one trailing zero. numbers = 720 numbers[0] , [1], [2]
+
+Example:
+Input:
+n = 4
+n = 5 (5*4*3*2*1 = 120) 
+Output:
+Number of trailing zeroes of factorial 4 is 0
+Number of trailing zeroes of factorial 5 is 1
+
+
+Q3.
+Write a program to count the total number of digit 1 appearing in all positive integers less than or equal to a given integer n. 
+Example:
+Input n = 12,
+Return 5, because digit 1 occurred 5 times in the following numbers: 1, 10, 11, 12.
+Example:
+Input:
+n = 12
+n = 30 (1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21)
+Output:
+Total number of digit 1 appearing in 12 (less than or equal) is 5.
+Total number of digit 1 appearing in 30 (less than or equal) is 13.
+
+'''
